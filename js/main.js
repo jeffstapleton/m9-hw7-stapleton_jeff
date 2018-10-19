d9a6272d7c9abccef6b2ad996c374b41ba5b6a8b
Jeff Stapleton <jeffstapleton@ufl.edu>
2018-10-19 16:55:22 -0500

    Duplicate existing code to modify for 2nd button

--- js/main.js
+++ js/main.js
@@ -9,3 +9,16 @@ function locateOut () {
 
 locateButton.addEventListener('mouseover', locateOver);
 locateButton.addEventListener('mouseout', locateOut);
+
+
+var locateButton = document.getElementById('location');
+
+function locateOver() {
+    locateButton.classList.add('darkgray');
+}
+function locateOut () {
+    locateButton.classList.remove('darkgray');
+}
+
+locateButton.addEventListener('mouseover', locateOver);
+locateButton.addEventListener('mouseout', locateOut);
