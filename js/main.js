e143f68cffae7e2a783f3707811f4d976b36a463
Jeff Stapleton <jeffstapleton@ufl.edu>
2018-10-19 16:57:24 -0500

    Change all new 'locate' code to 'order' code

--- js/main.js
+++ js/main.js
@@ -11,14 +11,14 @@ locateButton.addEventListener('mouseover', locateOver);
 locateButton.addEventListener('mouseout', locateOut);
 
 
-var locateButton = document.getElementById('location');
+var orderButton = document.getElementById('order');
 
-function locateOver() {
-    locateButton.classList.add('darkgray');
+function orderOver() {
+    orderButton.classList.add('darkgray');
 }
-function locateOut () {
-    locateButton.classList.remove('darkgray');
+function orderOut () {
+    orderButton.classList.remove('darkgray');
 }
 
-locateButton.addEventListener('mouseover', locateOver);
-locateButton.addEventListener('mouseout', locateOut);
+orderButton.addEventListener('mouseover', orderOver);
+orderButton.addEventListener('mouseout', orderOut);
